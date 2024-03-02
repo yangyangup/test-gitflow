69260deb116db1d8d63bc1fc6c1aee0f4622ae3f
Marvel <30824137+yangyangup@users.noreply.github.com>
2024-03-02 22:03:17 +0800

    2-commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello world!");
     }
 }
